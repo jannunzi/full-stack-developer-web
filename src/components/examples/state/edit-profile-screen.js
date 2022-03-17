12ee2f2afaae22edab96da594aeebd45732b672b
jannunzi <jannunzi@gmail.com>
2022-03-17 13:23:06 -0400

    profile

--- src/components/examples/state/edit-profile-screen.js
+++ src/components/examples/state/edit-profile-screen.js
@@ -2,6 +2,15 @@ import spacex from "./profile.json";
 import {useState} from "react";
 const EditProfileScreen = () => {
   const [profile, setProfile] = useState(spacex);
+  const toggleYourTuits = (event, yourTuits) => {
+    let newProfile = {...profile};
+    if(!event.target.checked) {
+      delete newProfile.yourTuits[yourTuits];
+    } else {
+      newProfile.yourTuits[yourTuits] = true;
+    }
+    setProfile(newProfile);
+  }
   return(
     <div>
       <h2>Edit Profile</h2>
@@ -35,45 +44,70 @@ const EditProfileScreen = () => {
         <option value="ENGINEER">Engineer</option>
         <option value="WELDER">Welder</option>
       </select>
-      <b>Email Notifications</b>
-      <div className="form-check form-switch">
-        <input className="form-check-input" type="checkbox" role="switch"
-               id="flexSwitchCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-            Default
-            switch checkbox input</label>
-      </div>
+      
+      
+      {/*<div className="form-check form-switch mb-3 ps-0">*/}
+      {/*  <input type="checkbox"*/}
+      {/*         onChange={event => setProfile(*/}
+      {/*    {...profile, emailNotifications: event.target.checked})}*/}
+      {/*         className="form-check-input float-end"*/}
+      {/*         role="switch"/>*/}
+      {/*    Email notifications*/}
+      {/*</div>*/}
+      <b>Notification Preferences</b><br/>
+      <b className="mt-5">Related to your Tuits</b>
       <div>
-        <input type="checkbox" className="float-end"/>
+        <input type="checkbox" checked={profile.yourTuits["NEW"]}
+               onClick={(e) => toggleYourTuits(e, 'NEW')}
+               name="your-tuits" className="float-end"/>
         New notifications
       </div>
       <div>
-        <input type="checkbox" className="float-end"/>
+        <input type="checkbox" checked={profile.yourTuits["DIRECT"]}
+               onClick={(e) => toggleYourTuits(e, 'DIRECT')}
+               name="your-tuits" className="float-end"/>
         Direct messages
       </div>
-      <div>
-        <input type="checkbox" className="float-end"/>
+      <div className="mb-3">
+        <input type="checkbox" checked={profile.yourTuits["EMAILED"]}
+               onClick={(e) => toggleYourTuits(e, 'EMAILED')}
+               name="your-tuits" className="float-end"/>
         Tweets emailed to you
       </div>
 
+
+
+      <b>Notification Preferences</b><br/>
+      <b className="mt-5">Top Tuits</b>
       <div>
-        <input type="radio" className="float-end"/>
+        <input type="radio" checked={profile.topTuits === 'DAILY'}
+               onClick={(e) => setProfile(
+                 {...profile, topTuits: 'DAILY'})}
+               name="topTuits" className="float-end"/>
         Daily
       </div>
       <div>
-        <input type="radio" className="float-end"/>
+        <input type="radio" checked={profile.topTuits === 'WEEKLY'}
+               onClick={(e) => setProfile(
+                 {...profile, topTuits: 'WEEKLY'})}
+               name="topTuits" className="float-end"/>
         Weekly
       </div>
       <div>
-        <input type="radio" className="float-end"/>
+        <input type="radio" checked={profile.topTuits === 'PERIODICALLY'}
+               onClick={(e) => setProfile(
+                 {...profile, topTuits: 'PERIODICALLY'})}
+               name="topTuits" className="float-end"/>
         Periodically
       </div>
       <div>
-        <input type="radio" className="float-end"/>
+        <input type="radio" checked={profile.topTuits === 'OFF'}
+               onClick={(e) => setProfile(
+                 {...profile, topTuits: 'OFF'})}
+               name="topTuits" className="float-end"/>
         Off
       </div>
-      {profile.dob}
-      {profile.role}
+      {JSON.stringify(profile.topTuits)}
     </div>
   );
 }
